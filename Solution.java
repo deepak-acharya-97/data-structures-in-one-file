dd654e1838070a2d36e137b22a4de35dc7a1c34d
Deepak Acharya <aacharyadeepak100@gmail.com>
2020-02-15 21:18:01 +0530

    Make code generic and Implement LinkedList

--- Solution.java
+++ Solution.java
@@ -1,12 +1,55 @@
 public class Solution {
-    public static class Node {
-        public int data;
+    public static class Node<T> {
+        public T data;
         public Node next;
 
-        public Node(int data) {
+        public Node(T data) {
             this.data = data;
             this.next = null;
         }
+    }
+
+    public static class LinkedList<T> {
+        public Node<T> head;
+
+        public LinkedList() {
+            this.head = null;
+        }
+
+        public void add(T data) {
+            if (head == null)
+                head = new Node(data);
+            else {
+                Node temp = head;
+                while (temp.next != null)
+                    temp = temp.next;
+                temp.next = new Node(data);
+            }
+        }
+
+        public void addFront(T data) {
+            if (head == null)
+                head = new Node(data);
+            else {
+                Node newNode = new Node(data);
+                newNode.next = head;
+                head = newNode;
+            }
+        }
+
+        public T deleteFront() {
+            if (head == null)
+                return new T();
+            if (head.next == null) {
+                T data = head.data;
+                head = null;
+                return data;
+            }
+            T data = head.data;
+            head = head.next;
+            return data;
+        }
+
     }
 
     public static void main(String[] args) {
