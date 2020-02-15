554b8ecc21e649a53e5eb66c2f3ceda9ded54a20
Deepak Acharya <aacharyadeepak100@gmail.com>
2020-02-15 20:01:10 +0530

    Create Node Class

--- Solution.java
+++ Solution.java
@@ -1,7 +1,15 @@
-public class Solution
-{
-    public static void main(String[] args)
-    {
-        
+public class Solution {
+    public static class Node {
+        public int data;
+        public Node next;
+
+        public Node(int data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
+
+    public static void main(String[] args) {
+
     }
 }
